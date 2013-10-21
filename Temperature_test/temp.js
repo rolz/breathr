d178fa2acbb783b62159fbcbe5254c6ca031b5d5
Tracy Alm <tracyalm1@gmail.com>
2013-10-21 15:11:21 +0200

    added water temperature, but not working.

--- Temperature_test/temp.js
+++ Temperature_test/temp.js
@@ -1,36 +1,72 @@
 $(document).ready(function() {
 
-function getData (api) {
+//http://api.worldweatheronline.com/free/v1/weather.ashx?q=stockholm&format=json&num_of_days=1&callback=%3F&key=f932x2a5kct424atxm6ned9z
 
-//http://api.worldweatheronline.com/free/v1/weather.ashx?q=stockholm&format=json&num_of_days=1&callback=?&key=86pdf5p7sa34xzrgbenmtjnb
-
-	var key = "86pdf5p7sa34xzrgbenmtjnb";
+function getWeatherData (api) {
 	var url = "http://api.worldweatheronline.com/free/v1/weather.ashx?q=";
 	var city = "Stockholm";
-	var numDays = 1;
-	var amazingCallback = "?"
 //	var coords = "";
+	var numDays = 1;
 	var format = "json";
+	var amazingCallback = "?"
+	var key = "f932x2a5kct424atxm6ned9z";
 
+	var fullQueryUrl = url+city+"&format="+format+"&num_of_days="+numDays+"&Callback="+amazingCallback+"&key="+key;
 
-
-
-	var fullQueryUrl = url+city+"&format="+format+"&num_of_days="+numDays+"&callback="+amazingCallback+"&key="+key;
-
-	//console.log(fullQueryUrl);
+	console.log(fullQueryUrl);
 
 	$.getJSON(fullQueryUrl, getTempInfo);
+	
 };
 
 
 
 function getTempInfo (weatherData) {
-		var currentTemperature = weatherData.data.current_condition[0].temp_C;
-		var city = weatherData.data.request[0].query;
-		var celsius = String.fromCharCode(176)+"C";
-		console.log("the temperature in "+city+" is "+currentTemperature+celsius);
+	var currentTemperature = weatherData.data.current_condition[0].temp_C;
+	var city = weatherData.data.request[0].query.substring(0,9); 
+	var celsius = String.fromCharCode(176)+"C";
+	var weatherDesc = weatherData.data.current_condition[0].weatherDesc[0].value;
+	var weatherCode = weatherData.data.current_condition[0].weatherCode;
+
+	console.log("It is "+weatherDesc+" and the temperature in "+city+" is "+currentTemperature+celsius); 
+	
+	
+	}
+
+getWeatherData();
+
+//Marineweather
+//http://api.worldweatheronline.com/free/v1/marine.ashx?q=38.88944%2C-77.03533&format=json&callback=%3F&key=f932x2a5kct424atxm6ned9z
+
+function getWaterData (waterApi) {
+	var url = "http://api.worldweatheronline.com/free/v1/marine.ashx?q=";
+	var q = "38.88944,-77.03533";
+	var format = "json";
+	var marineCallback = "?";
+	var key = "f932x2a5kct424atxm6ned9z";
+
+var fullQueryUrl = url+q+"&format="+format+"&Callback="+marineCallback+"&key="+key;
+
+	console.log(fullQueryUrl);
+
+	$.getJSON(fullQueryUrl, getWaterData);
+	
 };
 
-getData();
+function getTempWater (waterTemp) {
+	var waterTemperature = waterTemp.data.request.weather[0].waterTemp_C;
+	var q = waterData.data.request[0].query; 
+	var celsius = String.fromCharCode(176)+"C";
 
+	console.log("It is "+waterTemperature+" and the temperature in "+q+" is "+waterTemperature+celsius); 
+	
+	
+	}
+
+getWaterData();
+
+
+//
 });
+
+
