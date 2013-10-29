30756f28f7bc134ab0b294886028004422750d39
Zac Rolland <zac@barpass.co>
2013-10-30 00:01:26 +0100

    added directions to map but css still broken

--- App/js/map.js
+++ App/js/map.js
@@ -1,26 +1,36 @@
 var map = L.mapbox.map('map', 'barnaszasz.map-5d7s1o82');
 
-map.locate({setView: true, maxZoom: 13});
+map.locate({
+    setView: true,
+    maxZoom: 13
+});
+
+var userMarker;
 
 function onLocationFound(e) {
-    var radius = e.accuracy / 2;
-    
+
     var userIcon = L.icon({
         iconUrl: 'img/user_icon.png',
         iconSize: [33, 49],
-        iconAnchor: [33, 49]
+        iconAnchor: [16.5, 49],
+        popupAnchor: [0, -35]
     });
 
-    var userMarker = L.marker(e.latlng, {
+    userMarker = L.marker(e.latlng, {
         icon: userIcon,
-        draggable: true
-    }); 
-        
-    L.circle(e.latlng, radius).addTo(map);
-    
+        draggable: true,
+        zIndexOffset: 2
+    });
+
+    //    L.circle(e.latlng, radius).addTo(map);
+
     userMarker.addTo(map);
-    userMarker.bindPopup("You are within " + radius + " meters from this point").openPopup();
+    userMarker.bindPopup("<b>You are here!</b><br>Drag the market to find closest spot</br>").openPopup();
+
+    drag(userMarker);
+
 }
+
 
 map.on('locationfound', onLocationFound);
 
@@ -30,135 +40,219 @@ function onLocationError(e) {
 
 map.on('locationerror', onLocationError);
 
+
 var distancesLeftToCalculate = 0;
 
+function drag(userMarker) {
+    // user marker get coords on drag end
+    userMarker.on('dragend', function (event) {
+
+        var userMarkerLoc = userMarker.getLatLng(); // but using the passed event is cleaner
+        var userMarkerTransform = ('fuck' + userMarkerLoc);
+        var userMarkerCoords = userMarkerTransform.slice(10);
+
+        var _destinationSet = [];
+
+        // get distances between origin and destinations
+        function getDistances(destinationCoords, destinationMedia) {
+
+            var service = new google.maps.DistanceMatrixService();
+            service.getDistanceMatrix({
+                origins: [userMarkerCoords],
+                destinations: [destinationCoords],
+                travelMode: google.maps.TravelMode.WALKING,
+                unitSystem: google.maps.UnitSystem.METRIC
+            }, callback);
+
+            function callback(response, status) {
+
+                if (status == google.maps.DistanceMatrixStatus.OK) {
+                    var origins = response.originAddresses;
+                    var destinations = response.destinationAddresses;
+
+                    for (var i = 0; i < origins.length; i++) {
+                        var results = response.rows[i].elements;
+                        for (var j = 0; j < results.length; j++) {
+                            var element = results[j];
+                            var distance = element.distance.text;
+                            var duration = element.duration.text;
+                            //get time value
+                            var timeValue = element.duration.value;
+                            var from = origins[i];
+                            var to = destinations[j];
+
+                            // add needed elements to an object
+                            var proximityData = {
+                                distance: distance,
+                                duration: duration,
+                                timeValue: timeValue,
+                                from: from,
+                                to: to,
+                                originCoords: userMarkerCoords,
+                                destinationCoords: destinationCoords,
+                                destinationMedia: destinationMedia
+                            };
+
+                            //destinationsValuesArray.push(proximityData);
+                            _destinationSet.push(proximityData);
+                        }
+                    }
+                }
+
+                distancesLeftToCalculate--;
+                if (distancesLeftToCalculate <= 0) {
+                    getClosest(_destinationSet);
+                }
+            }
+        }
+        // get destinations data
+        function getDestinationsCoords(destinationCoordsData) {
+            distancesLeftToCalculate = destinationCoordsData.rows.length;
+            for (i = 0; i < destinationCoordsData.rows.length; i++) {
+                var raw = destinationCoordsData.rows[i];
+                var destinationCoords = new google.maps.LatLng(raw[3], raw[4]);
+                var destinationMedia = raw[7];
+
+                getDistances(destinationCoords, destinationMedia);
+            }
+        }
+
+        // get points from fusion table
+        function getDestinationsData() {
+            //***use urlEncode***
+            var fusionUrl = "https://www.googleapis.com/fusiontables/v1/";
+            var allDataQuery = "query?sql=SELECT%20*%20FROM%20";
+            var fusionTableId = "1uljVsKPiMm45Sjs41B6KHlXmvoa8STcU8p-dCLE";
+            var fusionKey = "&key=AIzaSyA8z1sLokJyNX3IX58jbSic-coCPpkKifM";
+
+            var url = fusionUrl + allDataQuery + fusionTableId + fusionKey;
+
+            $.getJSON(url, getDestinationsCoords);
+            console.log(url);
+        }
+
+        getDestinationsData();
+
+
+    })
+}
+
+//return closest destination **** find way to connect getDistances
+function getClosest(destinationSet) {
+
+    var index = 0;
+    var value = destinationSet[0].timeValue;
+
+    for (var i = 1; i < destinationSet.length; i++) {
+        if (destinationSet[i].timeValue < value) {
+            value = destinationSet[i].timeValue;
+            index = i;
+        }
+    }
+
+    var closestDestination = destinationSet[index];
+    //closest destination data set
+    console.log("this is closest ", closestDestination);
+
+    createDestinationMarkers(closestDestination);
+
+}
+
+// destination icon
 var destIcon = L.icon({
     iconUrl: 'img/dest_icon.png',
     iconSize: [35, 49],
-    iconAnchor: [20, 49]
+    iconAnchor: [20, 49],
+    popupAnchor: [0, -35]
 });
 
+//create destination markers
+function createDestinationMarkers(properties) {
+    var marker = L.marker([properties.destinationCoords.lb, properties.destinationCoords.mb], {
+        icon: destIcon,
+        zIndexOffset: 1
+    }).addTo(map).bindPopup("Click Here!").openPopup().on('click', function clickDestination() {
 
-// user marker get coords on drag end
-userMarker.on('dragend', function (event) {
+        console.log(properties.destinationMedia);
+        getBg(properties.destinationMedia);
+    });
 
-    var userMarkerLoc = userMarker.getLatLng(); // but using the passed event is cleaner
-    var userMarkerTransform = ('fuck' + userMarkerLoc);
-    var userMarkerCoords = userMarkerTransform.slice(10);
+    calcDirections(properties.originCoords, properties.destinationCoords);
+}
 
-    console.log(userMarkerCoords);
+function getBg(imgDestination) {
 
-    var _destinationSet = [];
+    var imgUrl = imgDestination;
+    console.log("two " + imgUrl);
 
-    // get distances between origin and destinations    
-    function getDistances(destinationCoords) {
+    // fade this in:
+    $('#img-container').css({
+        'background': 'url(' + imgUrl + ')',
+        'backgroundSize': '100%',
+        'display': 'block'
+    });
+}
 
-        var service = new google.maps.DistanceMatrixService();
-        service.getDistanceMatrix({
-            origins: [userMarkerCoords],
-            destinations: [destinationCoords],
-            travelMode: google.maps.TravelMode.WALKING,
-            unitSystem: google.maps.UnitSystem.METRIC
-        }, callback);
-
-        function callback(response, status) {
-
-            if (status == google.maps.DistanceMatrixStatus.OK) {
-                var origins = response.originAddresses;
-                var destinations = response.destinationAddresses;
-
-                for (var i = 0; i < origins.length; i++) {
-                    var results = response.rows[i].elements;
-                    for (var j = 0; j < results.length; j++) {
-                        var element = results[j];
-                        var distance = element.distance.text;
-                        var duration = element.duration.text;
-                        //get time value   
-                        var timeValue = element.duration.value;
-                        var from = origins[i];
-                        var to = destinations[j];
-
-                        // add needed elements to an object 
-                        var proximityData = {
-                            distance: distance,
-                            duration: duration,
-                            timeValue: timeValue,
-                            from: from,
-                            to: to,
-                            originCoords: userMarkerCoords,
-                            destinationCoords: destinationCoords
-                        };
-
-                        //destinationsValuesArray.push(proximityData); 
-                        _destinationSet.push(proximityData);
-                        console.log(proximityData);
-                    }
-                }
-            }
-            console.log(distancesLeftToCalculate)
-            distancesLeftToCalculate--;
-            if (distancesLeftToCalculate <= 0) {
-                console.log("All done mother fucker.")
-                getClosest(_destinationSet);
-            }
-        }
-    }
-
-    //return closest destination **** find way to connect getDistances  
-    function getClosest(destinationSet) {
-
-        var index = 0;
-        var value = destinationSet[0].timeValue;
-
-        for (var i = 1; i < destinationSet.length; i++) {
-            if (destinationSet[i].timeValue < value) {
-                value = destinationSet[i].timeValue;
-                index = i;
-            }
-        }
-
-        var closestDestination = destinationSet[index];
-        //closest destination data set 
-        console.log("this is closest ", closestDestination);
-
-        createDestinationMarkers(closestDestination);
-
-    }
+//add directions
 
 
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
 
-    //create destination markers    
-    function createDestinationMarkers(properties) {
-        var marker = L.marker([properties.destinationCoords.lb, properties.destinationCoords.mb], {
-            icon: destIcon
-        }).addTo(map);
+function calcDirections(originCoords, destinationCoords) {
+
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    $('#sidr-right').empty();
+    directionsDisplay.setPanel(document.getElementById('sidr-right'));
+
+    var start = originCoords;
+
+    var end = destinationCoords;
+
+    var request = {
+        origin: start,
+        destination: end,
+        travelMode: google.maps.TravelMode.WALKING
     };
-
-    // get destinations data    
-    function getDestinationsCoords(destinationCoordsData) {
-        distancesLeftToCalculate = destinationCoordsData.rows.length;
-        for (i = 0; i < destinationCoordsData.rows.length; i++) {
-            var raw = destinationCoordsData.rows[i];
-            var destinationCoords = new google.maps.LatLng(raw[3], raw[4]);
-
-            getDistances(destinationCoords);
+    directionsService.route(request, function (response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
         }
-    }
+    });
+}
 
-    // get points from fusion table    
-    function getDestinationsData() {
-        //***use urlEncode***     
-        var fusionUrl = "https://www.googleapis.com/fusiontables/v1/";
-        var allDataQuery = "query?sql=SELECT%20*%20FROM%20";
-        var fusionTableId = "1uljVsKPiMm45Sjs41B6KHlXmvoa8STcU8p-dCLE";
-        var fusionKey = "&key=AIzaSyA8z1sLokJyNX3IX58jbSic-coCPpkKifM";
+//add sidr
 
-        var url = fusionUrl + allDataQuery + fusionTableId + fusionKey;
-
-        $.getJSON(url, getDestinationsCoords);
-        console.log(url);
-    }
-
-    getDestinationsData();
-
+$('#right-menu').sidr({
+    name: 'sidr-right',
+    side: 'right',
 });
+
+function StartViewModel() {
+    // Data
+    var self = this;
+    self.temp = ko.observable();
+    self.marineTemp = ko.observable();
+    self.weatherIcon = ko.observable();
+    self.marineIcon = "img/waves.png";
+    self.walking = "img/walking_path.png";
+
+    var celcius = String.fromCharCode(176) + "C";
+
+    var weatherApi = "http://api.worldweatheronline.com/free/v1/weather.ashx?q=Stockholm&format=json&num_of_days=1&callback=?&key=86pdf5p7sa34xzrgbenmtjnb";
+    var marineApi = "http://api.worldweatheronline.com/free/v1/marine.ashx?q=59.329,18.06511&format=json&Callback=?&key=f932x2a5kct424atxm6ned9z";
+
+    var data = $.getJSON(weatherApi, function (allData) {
+        var t = allData.data.current_condition[0].temp_C + celcius;
+        var wC = allData.data.current_condition[0].weatherCode;
+        var climacon = "img/climacons/" + wC + ".png";
+        self.weatherIcon(climacon);
+        self.temp(t);
+    });
+
+    var mData = $.getJSON(marineApi, function (allData) {
+        var mT = allData.data.weather[0].hourly[0].waterTemp_C + celcius;
+        self.marineTemp(mT);
+    });
+}
+ko.applyBindings(new StartViewModel()); //key elements
