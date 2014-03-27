062fcdc80709db09ba31040f68f62b55234ca525
rolz <zac.rolland@hyperisland.se>
2014-03-27 12:29:00 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -171,7 +171,7 @@ var destIcon = L.icon({
 
 //create destination markers
 function createDestinationMarkers(properties) {
-    var marker = L.marker([properties.destinationCoords.b, properties.destinationCoords.d], {
+    var marker = L.marker([properties.destinationCoords.k, properties.destinationCoords.A], {
         icon: destIcon,
         zIndexOffset: 1
     }).addTo(map).bindPopup("<b>Click here to see the view!</b>").openPopup().on('click', function clickDestination() {
