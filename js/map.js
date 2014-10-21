7e47271e02db6321a972f73fccdd59f4ad39ae60
Zachary Rolland <zac.rolland@hyperisland.se>
2014-10-20 18:07:39 -0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -30,7 +30,7 @@ var newLatLng = new L.LatLng(lat, lng);
 
     userMarker.addTo(map);
     
-    userMarker.bindPopup("<b>You are here!</b><br>Drag the market to find closest spot</br>").openPopup();
+    userMarker.bindPopup("<b>You are here!</b><br>Drag the marker to find closest spot</br>").openPopup();
 
     drag(userMarker);
 
